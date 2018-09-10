fa46743cfb8458d7bfe5b7fb52f26076d6f314ad
Santanu09 <43047324+Santanu09@users.noreply.github.com>
2018-09-10 14:11:08 -0600

    added moving ract

---
+++ p5-1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  createCanvas(windowwidth, windowheight);
+  background(255,0,0);
+}
+function draw() {
+
+  fill(0, 255, 50);
+  rect(mouseX,mouseY,50,250);
+}
