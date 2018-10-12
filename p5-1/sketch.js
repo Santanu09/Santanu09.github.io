bf0304f7a6cd6e24244984fa59550d5bdf458933
Santanu09 <43047324+Santanu09@users.noreply.github.com>
2018-10-12 12:41:26 -0600

    f

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -1,13 +1,81 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// global variables
+let x, y, radius;
+let dx, dy;
+let dvd, dvdColor;
+let state;
+
+function preload() {
+  dvd = loadImage("assets/220px-FraiseFruitPhoto.jpg");
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  x = width / 2;
+  y = height / 2;
+  radius = 50;
+  dx = random(2, 5);
+  dy = random(2, 5);
+  dvdColor = color(0);
+  state = 1;
 }
 
 function draw() {
-  createCanvas(windowwidth, windowheight);
-  background(255,0,0);
+  background(255);
+  if (state === 1) {
+    displayStartScreen();
+  }
+  if (state === 2) {
+    moveThing();
+    displayThing();
+  }
 }
-function draw() {
 
-  fill(0, 255, 50);
-  rect(mouseX,mouseY,50,250);
+function displayStartScreen() {
+  let buttonWidth = 400;
+  let buttonHeight = 200;
+  let leftSide = width / 2 - buttonWidth / 2;
+  let topSide = height / 2 - buttonHeight / 2;
+  let rightSide = leftSide + buttonWidth;
+  let bottomSide = topSide + buttonHeight;
+
+  fill(0);
+  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    fill(125);
+    if (mouseIsPressed) {
+      state = 2;
+    }
+  }
+
+  rect(leftSide, topSide, buttonWidth, buttonHeight);
+}
+
+function moveThing() {
+  x += dx;
+  y += dy;
+
+  //check if bounce required
+  if (y + dvd.height / 2 >= height || y - dvd.height / 2 <= 0) {
+    dy = dy * -1;
+    dvdColor = color(random(255), random(255));
+  }
+
+  if (x + dvd.width / 2 >= width || x - dvd.width / 2 <= 0) {
+    dx = dx * -1;
+    dvdColor = color(random(255), random(255));
+  }
+}
+
+function displayThing() {
+  fill(0);
+  // ellipse(x, y, radius * 2, radius * 2);
+  imageMode(CENTER);
+  tint(dvdColor);
+  image(dvd, x, y);
 }
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,73 +1,81 @@
-// Project Title
+// 
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+// global variables
 
-let cat;
-let x, y;
+let x, y, radius;
 let dx, dy;
-let spellSound;
-let backgroundMusic;
-
+let dvd, dvdColor;
+let state;
 
 function preload() {
-  backgroundMusic = loadSound("assets/mlg.mp3");
-  spellSound = loadSound("assets/hitmaker.mp3");
-  cat = loadImage("assets/cat.png");
+  dvd = loadImage("images/logo.png");
 }
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2 - cat.width / 2;
-  y = height/2 - cat.height / 2;
-  dx = random(3, 8);
-  dy = random(3, 8);
-  strokeWeight(2);
-  backgroundMusic.setVolume(0.2);
-  backgroundMusic.loop();
+  x = width / 2;
+  y = height / 2;
+  radius = 50;
+  dx = random(2, 5);
+  dy = random(2, 5);
+  dvdColor = color(0);
+  state = 1;
 }
 
 function draw() {
-  background(random(255),random(255),random(255));
-  movecat();
-  displaycat();
-  if (mouseIsPressed){
-    stroke(255);
+  background(255);
+  if (state === 1) {
+    displayStartScreen();
   }
-  else {
-    stroke(237,34,93);
-  }
-  line(mouseX-66, mouseY, mouseX+66, mouseY);
-  line(mouseX, mouseY-66, mouseX, mouseY+66);
-  if (keyIsDown === "27") {
-    backgroundMusic.setVolume(0.2);
-    backgroundMusic.loop();
+  if (state === 2) {
+    moveThing();
+    displayThing();
   }
 }
 
-function movecat() {
-  // apply speed
+function displayStartScreen() {
+  let buttonWidth = 400;
+  let buttonHeight = 200;
+  let leftSide = width / 2 - buttonWidth / 2;
+  let topSide = height / 2 - buttonHeight / 2;
+  let rightSide = leftSide + buttonWidth;
+  let bottomSide = topSide + buttonHeight;
+
+  fill(0);
+  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    fill(125);
+    if (mouseIsPressed) {
+      state = 2;
+    }
+  }
+
+  rect(leftSide, topSide, buttonWidth, buttonHeight);
+}
+
+function moveThing() {
   x += dx;
   y += dy;
 
-  //check for bounce
-  if (x + cat.width >= width || x <= 0) {
-    dx = dx * -1;
-  }
-  if (y + cat.height >= height || y <= 0) {
+  //check if bounce required
+  if (y + dvd.height / 2 >= height || y - dvd.height / 2 <= 0) {
     dy = dy * -1;
+    dvdColor = color(random(255), random(255));
+  }
+
+  if (x + dvd.width / 2 >= width || x - dvd.width / 2 <= 0) {
+    dx = dx * -1;
+    dvdColor = color(random(255), random(255));
   }
 }
 
-function displaycat() {
-  image(cat, x, y);
-}
-
-function mousePressed() {
-  background(0);
-  noStroke();
-  spellSound.play();
+function displayThing() {
+  fill(0);
+  // ellipse(x, y, radius * 2, radius * 2);
+  imageMode(CENTER);
+  tint(dvdColor);
+  image(dvd, x, y);
 }
